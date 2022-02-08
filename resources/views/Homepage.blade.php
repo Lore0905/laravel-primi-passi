4f74e1d50e642804daeb33ae7905c3d072a3c1e0
Lore0905 <lore.castelli4@gmail.com>
2022-02-08 15:45:46 +0100

    'Hello World' without dinamic data

---
+++ resources/views/Homepage.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Home Page</title>
+</head>
+<body>
+    <style>
+        h1{
+            color:red;
+            padding: 50px;
+            background-color: black;
+            text-align: center
+        }
+    </style>
+    <div>
+        <h1>Hello World</h1>
+    </div>
+    
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,13 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+// A questo punto, iniziamo a prendere confidenza con le rotte e le views: cancelliamo la view welcome.blade.php
+//  e creiamo una nostra homepage. Facciamo quindi sì che la rotta / visualizzi home.blade.php
+// Inizialmente stampiamo un Hello World, poi passiamo dei dati alla view in modo da visualizzarli dinamicamente con Blade.
+// Bonus:
+// Creiamo più di una pagina e visualizziamo un header menu con i link di tutte le pagine, utilizzando la funzione route()
 
 Route::get('/', function () {
-    return view('welcome');
+    
+    return view('Homepage');
 });
